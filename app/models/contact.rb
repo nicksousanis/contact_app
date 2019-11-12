98d682c1a9d52c5e9f4bf8ade9556a60c689388c
Nick <nsousanis14@yahoo.com>
2019-11-11 21:28:29 -0600

    Added partial json file

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,13 @@
 class Contact < ApplicationRecord
+  def friendly_updated_at
+    friendly_updated_at = updated_at.strftime("%B %e, %Y, %k:%M:%S")
+  end
+
+  def full_name
+    full_name = first_name + " " + last_name
+  end
+
+  def japenese_phone_number
+    japenese_number = "+81-" + phone_number
+  end
 end
