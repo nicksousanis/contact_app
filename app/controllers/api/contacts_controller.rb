c12bbfc11e3bdb272ce3a14c060a4dea1b332e18
Nick <nsousanis14@yahoo.com>
2019-11-19 19:36:06 -0600

    Added user login in and verification

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,6 +1,10 @@
 class Api::ContactsController < ApplicationController
   def index
-    @contacts = Contact.all.order(:id)
+    @contacts = current_user.contacts
+
+    if params[:search]
+      @contacts = @contacts.where("first_name ILIKE ? OR last_name ILIKE ? OR email ILIKE ? OR phone_number ILIKE ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%")
+    end
     render "index.json.jb"
   end
 
@@ -12,22 +16,33 @@ class Api::ContactsController < ApplicationController
   def create
     @contact = Contact.new(
       first_name: params["first_name"],
+      middle_name: params["middle_name"],
       last_name: params["last_name"],
       email: params["email"],
       phone_number: params["phone_number"],
+      bio: params["bio"],
+      user_id: current_user.id,
     )
-    @contact.save
-    render "show.json.jb"
+    if @contact.save
+      render "show.json.jb"
+    else
+      render json: { errors: @contact.errors.full_messages }, status: 422
+    end
   end
 
   def update
     @contact = Contact.find_by(id: params["id"])
     @contact.first_name = params["first_name"] || @contact.first_name
+    @contact.middle_name = params["middle_name"] || @contact.middle_name
     @contact.last_name = params["last_name"] || @contact.last_name
     @contact.email = params["email"] || @contact.email
     @contact.phone_number = params["phone_number"] || @contact.phone_number
-    @contact.save
-    render "show.json.jb"
+    @contact.bio = params["bio"] || @contact.bio
+    if @contact.save
+      render "show.json.jb"
+    else
+      render json: { errors: @contact.errors.full_messages }, status: 422
+    end
   end
 
   def destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_023323) do
+ActiveRecord::Schema.define(version: 2019_11_20_004729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,17 @@ ActiveRecord::Schema.define(version: 2019_11_05_023323) do
     t.string "last_name"
     t.string "email"
     t.string "phone_number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "middle_name"
+    t.text "bio"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
